33018aa01e9d7d56170b3b3bfb284a66329f8b56
Yusuf Bata <yusuf.neobyte@gmail.com>
2014-11-19 22:55:09 +0200

    Added fest and mockito with application service test

---
+++ train-problem/src/test/java/za/co/thoughtworks/trains/RailroadServiceTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package za.co.thoughtworks.trains;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Yusuf
+ *
+ */
+public class RailroadServiceTest {
+
+	@Test
+	public void test() {
+		assertThat(false).isTrue();
+	}
+
+}
