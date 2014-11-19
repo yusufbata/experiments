7f70066729dd7fb69ca718b58e50b01aa89ea835
Yusuf Bata <yusuf.neobyte@gmail.com>
2014-11-19 22:37:49 +0200

    Add initial maven java project template

---
+++ train-problem/src/main/java/za/co/thoughtworks/trains/App.java
@@ -0,0 +1,12 @@
+package za.co.thoughtworks.trains;
+
+/**
+ * @author Yusuf
+ * 
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ train-problem/src/test/java/za/co/thoughtworks/trains/AppTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package za.co.thoughtworks.trains;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Yusuf
+ *
+ */
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
